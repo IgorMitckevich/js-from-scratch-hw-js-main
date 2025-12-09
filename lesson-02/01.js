897fd9ca218a6812e4a4142a309086229b432a84
IgorMitckevich <mickevich_elenaa@mail.ru>
2025-12-09 21:11:48 +0300

    i think i got my fault in lesson-02/01 and rework

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -8,20 +8,19 @@
 */
 
 // тестовые данные (значения можно менять)
-const isAdmin = false
-const isVerifiedUser = true
-const hasSpecialPermission = true
-const hasTemporaryPass = false
+const isAdmin = false;
+const isVerifiedUser = true;
+const hasSpecialPermission = true;
+const hasTemporaryPass = false;
 
-let isAccess
+let isAccess;
 
 // your code
 
-
-if(isAdmin || isVerifiedUser){
-    if(hasSpecialPermission || hasTemporaryPass ){
-       isAccess=true; 
-    }
-    
+if (isAdmin || isVerifiedUser) {
+  if (hasSpecialPermission || hasTemporaryPass) {
+    isAccess = true;
+  } else {
+    isAccess = false;
+  }
 }
-
