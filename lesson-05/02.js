18ce67467d6cc32267baaf83acc8efcb0e61271e
IgorMitckevich <mickevich_elenaa@mail.ru>
2025-12-27 16:17:17 +0300

    lesson-05/02 ready

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -35,4 +35,10 @@ const gallery = {
   'The Scream': 'Edvard Munch',
 }
 
-function updateGallery() {}
+function updateGallery(objGallery, nameGallery, newValue) {
+  
+  if(nameGallery in objGallery){
+    objGallery[nameGallery]=newValue;
+  }
+  
+}
